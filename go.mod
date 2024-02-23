f9d287e3c4e890f677f4d559d2077a5f83b6ff5c
zmh <siovanus@126.com>
2024-02-23 17:05:42 +0800

    update

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.20
 
 require github.com/btcsuite/btcd/btcec/v2 v2.3.2
 
-require github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+require (
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr/musig2"
 )
 
 const (
-	numSigners = 30
-	m          = 21
+	numSigners  = 7
+	m           = 5
+	numCommitee = 4
 )
 
 func main() {
-	// First generate the set of signers along with their public keys.
+	// First generate the set of multisign signers
 	signerKeys := make([]*btcec.PrivateKey, numSigners)
 	signSet := make([]*btcec.PublicKey, numSigners)
 	for i := 0; i < numSigners; i++ {
@@ -28,11 +30,46 @@ func main() {
 		signSet[i] = pubKey
 	}
 
+	// Generate four commitee signers
+	cSignerKeys := make([]*btcec.PrivateKey, numCommitee)
+	cSignSet := make([]*btcec.PublicKey, numCommitee)
+	for i := 0; i < numCommitee; i++ {
+		privKey, err := btcec.NewPrivateKey()
+		if err != nil {
+			panic(fmt.Sprintf("unable to gen priv key: %v", err))
+		}
+
+		pubKey := privKey.PubKey()
+
+		cSignerKeys[i] = privKey
+		cSignSet[i] = pubKey
+	}
+
 	timeStart := time.Now()
+
+	// C(numSigners, m)
 	indexs := combineResult(numSigners, m)
-	result := findByIndexs(signSet, indexs)
+	signerCombList := findByIndexs(signSet, indexs)
+
+	// Aggregate each combination, so we get aggregate key list
+	aggregatedKeyList := make([]*btcec.PublicKey, len(signerCombList))
+	for idx, signerComb := range signerCombList {
+		aggregatedKey, _, _, err := musig2.AggregateKeys(signerComb, false)
+		if err != nil {
+			panic(fmt.Sprintf("musig2.AggregateKeys error: %v", err))
+		}
+		aggregatedKeyList[idx] = aggregatedKey.FinalKey
+	}
+
+	// Gen all keys aggregate, use as internal key according to BIP341: https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki#constructing-and-spending-taproot-outputs
+	aggregatedAllKeys, _, _, err := musig2.AggregateKeys(append(cSignSet, signSet...), false)
+	if err != nil {
+		panic(fmt.Sprintf("all keys musig2.AggregateKeys error: %v", err))
+	}
+
+	// Gen taproot from aggregate key list
+
+	fmt.Println(aggregatedAllKeys)
 	timeEnd := time.Now()
-	fmt.Println("count:", len(result))
-	//fmt.Println("result:", result)
 	fmt.Println("time consume:", timeEnd.Sub(timeStart))
 }
